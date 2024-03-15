7f214745a17561d6c73e2efed57f9a0621c9bfa7
Anuj <anujkumar21052004@gmail.com>
2024-03-15 18:49:47 +0530

    Added Card Layout

---
+++ src/add-cards.js
@@ -0,0 +1,27 @@
+function addProject() {
+  const addProjectICon = document.querySelector("#add-project");
+  const addMoreProjectsIcon = document.querySelector(".add-more-projects");
+
+  addProjectICon.style.display = "none";
+  addMoreProjectsIcon.style.display = "flex";
+  content.style.display = "flex";
+
+  const cardDiv = document.createElement("div");
+  cardDiv.id = "card";
+
+  const projectTitle = document.createElement("div");
+  projectTitle.id = "projectTitle";
+  cardDiv.appendChild(projectTitle);
+
+  const dateDiv = document.createElement("div");
+  dateDiv.id = "dateDiv";
+  cardDiv.appendChild(dateDiv);
+
+  const completedIcon = document.createElement("div");
+  completedIcon.id = "completedIcon";
+  cardDiv.appendChild(completedIcon);
+
+  content.appendChild(cardDiv);
+}
+
+export { addProject };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+import "./style.css";
+import { addProject } from "./add-cards";
+
+const content = document.querySelector("#content");
+const addMoreProjectsIcon = document
+  .querySelector(".add-more-projects")
+  .addEventListener("click", addProject);
+const addProjectICon = document
+  .querySelector("#add-project")
+  .addEventListener("click", addProject);
